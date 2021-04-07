33639fa19b2320d871a5b27cd4fa5bc70143e1dd
Leo Niemi <niemi.leo@gmail.com>
2021-04-07 12:42:52 +0300

    Muutos databaseschema.sql:iin; lisätty ensi kommentti ja pari käyttäjää

--- databaseschema.sql
+++ databaseschema.sql
@@ -23,6 +23,7 @@ CREATE TABLE Comments (
     id SERIAL PRIMARY KEY,
     owner_id INT REFERENCES Users (id),
     task_id INT REFERENCES Tasks (id),
+    comment TEXT,
     time TIMESTAMP
 );
 
@@ -33,10 +34,10 @@ CREATE TABLE Follows (
 );
 
 INSERT INTO Users (username, password, admin) VALUES ('Olen.Leo', 'Basso', TRUE);
-INSERT INTO Tasksets (owner_id, name) VALUES (4, 'Tietokannan rakentelua');
-INSERT INTO Tasks (user_id, taskset_id, taskname, completed) VALUES (4,1, 'Esimerkkien lisäystä PSQL-tulkin kautta', CURRENT_TIMESTAMP);
+INSERT INTO Tasksets (owner_id, name) VALUES (1, 'Tietokannan rakentelua');
+INSERT INTO Tasks (user_id, taskset_id, taskname, completed) VALUES (1,1, 'Esimerkkien lisäystä PSQL-tulkin kautta', CURRENT_TIMESTAMP);
 INSERT INTO Users (username, password, admin) VALUES ('Leonkaveri', 'Basso', FALSE);
-INSERT INTO Follows (user1_id, user2_id) VALUES (5,4);
-INSERT INTO Comments (owner_id, task_id, comment, time) VALUES (5,1,'Hyvältä näyttää mutta eikös tämä ole huonosti totetutettu mock-tietokantaa ajatellen?', CURRENT_TIMESTAMP);
+INSERT INTO Follows (user1_id, user2_id) VALUES (2,1);
+INSERT INTO Comments (owner_id, task_id, comment, time) VALUES (2,1,'Hyvältä näyttää mutta eikös tämä ole huonosti totetutettu mock-tietokantaa ajatellen?', CURRENT_TIMESTAMP);
 
 
