835fe65dc528a0f215ce3502347617cf698ffc44
Leo Niemi <niemi.leo@gmail.com>
2021-05-06 19:30:56 +0300

    Korjattu bugi follows-luokassa

--- follows.py
+++ follows.py
@@ -2,8 +2,8 @@ from db import db
 
 def get_follows(userID):
 
-    sql = """SELECT username, users.id FROM users, follows WHERE users.id = follows.user2_id AND visible = 1;"""
-
+    #sql = """SELECT username, users.id FROM users, follows WHERE users.id :userID = follows.user1_id AND visible = 1;"""
+    sql = """SELECT DISTINCT username, users.id FROM users, follows WHERE user1_id = :userID AND user2_id = users.id AND visible = 1;"""
     return db.session.execute(sql, {"userID": userID}).fetchall()
 
 def unfollow(id, userID):
@@ -24,20 +24,20 @@ def follow(id, userID):
 
 #   CREATE OR REPLACE FUNCTION follow(_id int, _userID int) 
 #	    RETURNS void
-#	    language 'plpgsql'
+#       language 'plpgsql'
 #	    AS	      
 #	    $BODY$
 #	        BEGIN
-#	            IF (SELECT EXISTS(
- #                              SELECT 1 FROM follows 
- #                              WHERE user1_id = _userID 
-  #                             AND user2_id = _id 
-  #                             AND VISIBLE = 0)) 
-  #             THEN 
-  #                 UPDATE follows SET visible = 1 WHERE user1_id = _userID AND user2_id = _id;
-#	            ELSE
-##	                INSERT INTO follows (user1_id, user2_id) VALUES (_userID, _id);
-#	            END IF;
-	#        END;
+#                IF (SELECT EXISTS(
+#                    SELECT 1 FROM follows 
+#                        WHERE user1_id = _userID 
+#                        AND user2_id = _id 
+#                        AND (VISIBLE = 0 OR VISIBLE = 1))) 
+#                THEN 
+#                    UPDATE follows SET visible = 1 WHERE user1_id = _userID AND user2_id = _id;
+#                ELSE 
+#                    INSERT INTO follows (user1_id, user2_id) VALUES (_userID, _id);
+#        END IF;
+#        END;
 #	    $BODY$;
 
