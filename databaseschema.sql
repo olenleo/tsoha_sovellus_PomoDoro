a6c0189b5594dd51fb9b1f24aabf32dc26aa34d0
Leo Niemi <50656714+olenleo@users.noreply.github.com>
2021-05-07 12:54:25 +0300

    Update databaseschema.sql

--- databaseschema.sql
+++ databaseschema.sql
@@ -32,17 +32,3 @@ CREATE TABLE Follows (
     user1_id INT REFERENCES Users (id), 
     user2_id INT REFERENCES Users (id)
 );
-
-
-/*
-Testausta varten; tämä on poistettavissa. 
-TODO: admin-oikeuksilla ei vielä tee mitään, mutta tuo salasanan julkaisuhan ei ehkä ole ihan ok.
-*/
-INSERT INTO Users (username, password, admin) VALUES ('Olen.Leo', 'Basso', TRUE);
-INSERT INTO Tasksets (owner_id, name) VALUES (1, 'Tietokannan rakentelua');
-INSERT INTO Tasks (user_id, taskset_id, taskname, completed) VALUES (1,1, 'Esimerkkien lisäystä PSQL-tulkin kautta', CURRENT_TIMESTAMP);
-INSERT INTO Users (username, password, admin) VALUES ('Leonkaveri', 'Basso', FALSE);
-INSERT INTO Follows (user1_id, user2_id) VALUES (2,1);
-INSERT INTO Comments (owner_id, task_id, comment, time) VALUES (2,1,'Hyvältä näyttää?', CURRENT_TIMESTAMP);
-
-
